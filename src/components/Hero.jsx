288324b2945f39b266b6f1d8fae3b5b08e992ab6
Forrest Lin <forrest.lin.developer@gmail.com>
2023-03-20 22:52:58 +1100

    change slide button size, fix line change of home page

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -22,16 +22,15 @@ const Hero = () => {
             </span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I am a <span className="text-[#915eff]">Human Being</span> first,
-            <br className="sm:block hidden" />a{" "}
-            <span className="text-[#915eff]">Developer</span> second,
-            <br className="sm:block hidden" />
-            and a <span className="text-[#915eff]">Cellist</span> third.
+            I am a <span className="text-[#915eff]">Human Being </span>first,
+            <br />a <span className="text-[#915eff]">Developer </span>second,
+            <br />
+            and a <span className="text-[#915eff]">Cellist </span>third.
           </p>
         </div>
       </div>
       <ComputersCanvas />
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute bottom-10 w-full flex justify-center items-center">
         <a href="#about">
           <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.div
